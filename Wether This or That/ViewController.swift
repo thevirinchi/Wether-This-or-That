869c8764d82ac48ab70c1a2c78f6ca2e374e1765
z3t4z00k <zetazook@gmail.com>
2020-10-04 22:41:45 +0530

    first commit

---
+++ Wether This or That/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wether This or That
+//
+//  Created by Archit Agarwal on 04/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
